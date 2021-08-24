87225465df94f1d66fa394d1d51a478ccdb153c6
developerfaraz745 <muhammadfaraz745@gmail.com>
2021-08-24 21:30:04 +0500

    Page Commit.

---
+++ farazhashmi21.sh
@@ -0,0 +1,8 @@
+echo "Faraz Hashmi 21";
+echo $(git init);
+echo $(git add .);
+echo $(git commit -m "Page Commit.");
+echo $(git remote -v);
+echo $(git branch -M main);
+echo $(git push -u origin main);
+
